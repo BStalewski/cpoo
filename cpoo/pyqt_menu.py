3ef4e47ac0a0190ec00bb7168fd2364502a03fc3
Bartosz Stalewski <bartekstalewski@gmail.com>
2013-05-10 23:04:51 +0200

    Tworzenie akcji przeniesione do oddzielnej funkcji

--- cpoo/pyqt_menu.py
+++ cpoo/pyqt_menu.py
@@ -38,72 +38,70 @@ class Gui(QtGui.QMainWindow):
         file_menu = menubar.addMenu(u'&Plik')
         run_menu = menubar.addMenu(u'&Wykonaj')
 
-        open_image_action = QtGui.QAction(u'Otwórz obraz', self)
-        open_image_action.setShortcut(u'Ctrl+O')
-        open_image_action.setStatusTip(u'Wczytaj nowy obraz')
-        open_image_action.triggered.connect(self.open_image)
+        open_image_action = self.create_action(u'Otwórz obraz', u'Ctrl+O',
+                                               u'Wczytaj nowy obraz', self.open_image)
+        save_image_action = self.create_action(u'Zapisz obraz po segmentacji', u'Ctrl+S',
+                                               u'Zapisz przetworzony obraz', self.save_image, False)
+        exit_action = self.create_action(u'Exit', u'Ctrl+Q', u'Zakończ program', self.close)
+        file_menu.addActions([open_image_action, save_image_action, exit_action])
 
-        save_image_action = QtGui.QAction(u'Zapisz obraz po segmentacji', self)
-        save_image_action.setShortcut(u'Ctrl+S')
-        save_image_action.setStatusTip(u'Zapisz przetworzony obraz')
-        save_image_action.triggered.connect(self.save_image)
+        thresholding_action = self.create_action(u'Progowanie', u'Ctrl+P', u'Wykonaj algorytm progowania',
+                                                 self.thresholding, False)
+        ml_em_action = self.create_action(u'Algorytm ML-EM', u'Ctrl+M',
+                                          u'Wykonaj algorytm ML-EM', self.ml_em, False)
+        repeat_action = self.create_action(u'Powtórz ostatni algorytm', u'Ctrl+R',
+                                           u'Wykonaj ponownie poprzedni algorytm z tym samymi ustawieniami',
+                                           self.repeat, False)
+        run_menu.addActions([thresholding_action, ml_em_action, repeat_action])
 
-        exit_action = QtGui.QAction(u'Exit', self)
-        exit_action.setShortcut(u'Ctrl+Q')
-        exit_action.setStatusTip(u'Zakończ program')
-        exit_action.triggered.connect(self.close)
+        self.actions = {
+            u'open': open_image_action,
+            u'save': save_image_action,
+            u'exit': exit_action,
+            u'thresholding': thresholding_action,
+            u'ml_em': ml_em_action,
+            u'repeat': repeat_action,
+        }
 
-        file_menu.addAction(open_image_action)
-        file_menu.addAction(save_image_action)
-        file_menu.addAction(exit_action)
-
-        thresholding_action = QtGui.QAction(u'Progowanie', self)
-        thresholding_action.setShortcut(u'Ctrl+P')
-        thresholding_action.setStatusTip(u'Wykonaj algorytm progowania')
-        thresholding_action.triggered.connect(self.thresholding)
-
-        ml_em_action = QtGui.QAction(u'Algorytm ML-EM', self)
-        ml_em_action.setShortcut(u'Ctrl+M')
-        ml_em_action.setStatusTip(u'Wykonaj algorytm ML-EM')
-        ml_em_action.triggered.connect(self.ml_em)
-
-        repeat_action = QtGui.QAction(u'Powtórz ostatni algorytm', self)
-        repeat_action.setShortcut(u'Ctrl+R')
-        repeat_action.setStatusTip(u'Wykonaj ponownie poprzedni algorytm z tym samymi ustawieniami')
-        repeat_action.triggered.connect(self.repeat)
-
-        run_menu.addAction(thresholding_action)
-        run_menu.addAction(ml_em_action)
-        run_menu.addAction(repeat_action)
+    def create_action(self, name, shortcut, status_tip, callback, enabled=True):
+        action = QtGui.QAction(name, self)
+        action.setShortcut(shortcut)
+        action.setStatusTip(status_tip)
+        action.triggered.connect(callback)
+        action.setEnabled(enabled)
+        return action
 
     def open_image(self):
         self.src_fname = QtGui.QFileDialog.getOpenFileName(self, u'Wybierz obraz do segmentacji',
                                                            u'', u'Obrazy (*.png *.jpg)')
         pixmap = QtGui.QPixmap(self.src_fname)
         self.src_label.setPixmap(pixmap)
+        self.actions[u'thresholding'].setEnabled(True)
+        self.actions[u'ml_em'].setEnabled(True)
 
     def save_image(self):
         self.segmented_image_fname = QtGui.QFileDialog.getSaveFileName(self, u'Zapisz obraz po segmentacji',
                                                                        u'', u'Obrazy (*.png *.jpg)')
         self.segmented_image.save(self.segmented_image_fname)
 
-    def thresholding(self):
-        new_image = algorithms.thresholding(unicode(self.src_fname))
-        self.segmented_image = ImageQt.ImageQt(new_image)
+    def after_algorithm(self, segmented_image):
+        self.segmented_image = ImageQt.ImageQt(segmented_image)
         # if something bad happens here, try to uncomment
         #self.segmented_image = QtGui.QImage(self.segmented_image)
         segmented_image_pixmap = QtGui.QPixmap.fromImage(self.segmented_image)
         self.segmented_image_label.setPixmap(segmented_image_pixmap)
+        self.actions[u'save'].setEnabled(True)
+        self.actions[u'repeat'].setEnabled(True)
+
+    def thresholding(self):
+        new_image = algorithms.thresholding(unicode(self.src_fname))
         self.last_method = self.thresholding
+        self.after_algorithm(new_image)
 
     def ml_em(self):
         new_image = algorithms.ml_em(unicode(self.src_fname))
-        self.segmented_image = ImageQt.ImageQt(new_image)
-        # if something bad happens here, try to uncomment
-        #self.segmented_image = QtGui.QImage(self.segmented_image)
-        segmented_image_pixmap = QtGui.QPixmap.fromImage(self.segmented_image)
-        self.segmented_image_label.setPixmap(segmented_image_pixmap)
         self.last_method = self.ml_em
+        self.after_algorithm(new_image)
 
     def repeat(self):
         self.last_method()
